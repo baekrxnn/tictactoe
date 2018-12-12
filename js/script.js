8f18171d4dfa65a7281d85aa04447259224eb55f
Karen He <hainechoko@gmail.com>
2018-12-12 02:03:18 +0000

    counter is used to check the number of turns

--- js/script.js
+++ js/script.js
@@ -5,12 +5,11 @@
 let currentUser="o";
 
 let counter=0;
-    counter++;
+    
     
 function performLogic(buttonId,tileId) {
     $(buttonId).hide();
     $(tileId).text(player);
-    
 }
 
 function player() {
@@ -35,38 +34,55 @@ function player() {
     
 }
 
+function turns() {
+    let numberOfTurns=counter++;
+    
+    if (numberOfTurns===9) {
+        $("h1").text("draw!");
+    }
+}
+
 $("#button1").click( function() {
     performLogic("#button1","#tile1");
+    turns();
 });
 
 $("#button2").click( function() {
     performLogic("#button2","#tile2");
+    turns();
 });
 
 $("#button3").click( function() {
     performLogic("#button3","#tile3");
+    turns();
 });
 
 $("#button4").click( function() {
     performLogic("#button4","#tile4");
+    turns();
 });
 
 $("#button5").click( function() {
     performLogic("#button5","#tile5");
+    turns();
 });
 
 $("#button6").click( function() {
     performLogic("#button6","#tile6");
+    turns();
 });
 
 $("#button7").click( function() {
     performLogic("#button7","#tile7");
+    turns();
 });
 
 $("#button8").click( function() {
     performLogic("#button8","#tile8");
+    turns();
 });
 
 $("#button9").click( function() {
     performLogic("#button9","#tile9");
+    turns();
 });
